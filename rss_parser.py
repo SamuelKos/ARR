0c1ee975e04b3533a9eee1c0cf48ec9702fa9aa8
89200126+SamuelKos@users.noreply.github.com <89200126+SamuelKos@users.noreply.github.com>
2022-12-06 13:39:11 +0000

    give reader a name
    FossilOrigin-Name: ec2b29e2199bb114cc784c050fbc3ceae5131ede641cf21d805885fbcdd65014

---
+++ rss_parser.py
@@ -0,0 +1,95 @@
+# RSS specs:
+# https://www.rssboard.org/rss-profile#namespace-elements
+# Atom (blogs) specs:
+# http://www.atomenabled.org/developers/syndication/
+
+import urllib.request
+import html
+import re
+
+
+class Parser:
+	''' Simple parser for xml.
+	'''
+	
+	def __init__(self):
+		self.title_of_feed = None
+		self.data = {}
+		self.tags = ['title', 'link']# 'media:content', 'description']
+		self.user_agent = 'https://github.com/SamuelKos/ARR'
+		self.rss_types = {
+			'Atom':			'<entry>',
+			'RSS':			'<item>',
+			'Feedburner':	'xmlns:blogger='
+			}
+
+
+	def parse(self, link):
+		'''	link is URL-address to RSS-feed written in XML.
+			Returns tuple containing title of feed and two list: 
+			news-titles and links to news-page.
+		'''
+		req = urllib.request.Request(link)
+		req.add_header('User-Agent', self.user_agent)
+		
+		try: r = urllib.request.urlopen(req, timeout = 8)
+		except OSError: raise
+			
+		data_xml = html.unescape(r.read().decode('utf-8', 'ignore'))
+		
+		# Trying to apply some specs:
+		
+		if self.rss_types['Feedburner'] in data_xml:
+			ctag = 'entry'
+			patt_for_feedtitle = '<feed.*?<title.*?>(.*?)</title>'
+			patt_for_link = '''<entry>.*?</content>.*?<link.*?type=['"]text/html['"].*? href\\s*=.*?(h.*?)['"].*?/>.*?</entry>'''
+		
+		elif self.rss_types['RSS'] in data_xml:
+			ctag = 'item'
+			patt_for_feedtitle = '<channel>.*?<title.*?>(.*?)</title>'
+			patt_for_link = '<item>.*?<link.*?>(?:<\\!\\[CDATA\[)?(.*?)(?:\\]\\]>)?</link>.*?</item>'
+			
+		
+		elif self.rss_types['Atom'] in data_xml:
+			ctag = 'entry'
+			patt_for_feedtitle = '<feed.*?<title.*?>(.*?)</title>'
+			patt_for_link = '''<entry>.*?<link.*?href\\s*=.*?(h.*?)['"]*/>.*?</entry>'''
+			
+		else:
+			err_msg =	\
+'''rss_parser.py:
+
+Parsing error: unknown RSS-format
+'''
+			self.err = ValueError(err_msg)
+			raise self.err
+		
+		# Parse title of feed:
+		m = re.search(patt_for_feedtitle, data_xml, re.DOTALL)
+		self.title_of_feed = m.group(1)
+		
+		
+		# Parse tags:
+		for tag in self.tags:
+			self.data[tag] = list()
+			
+##			if tag == 'media:content':
+##				# grab first link found which hopefully is an image:
+##				pattern = f"<{ctag}>.*?<{tag}.*?url.*?=.*?\"(.*?)\""
+			
+			if tag == 'link':
+				pattern = patt_for_link
+				
+			elif tag == 'title':
+				pattern = f"<{ctag}>.*?<{tag}.*?>(?:<\\!\\[CDATA\[)?(.*?)(?:\\]\\]>)?</{tag}>.*?</{ctag}>"
+				
+			else:
+				pass
+			
+			self.data[tag] = re.findall(pattern, data_xml, flags=re.DOTALL)
+
+				
+		return (self.title_of_feed, self.data['title'], self.data['link'])
+		
+		
+
---
+++ rssfeed.py
@@ -0,0 +1,80 @@
+import rss_parser
+
+
+class RssFeed:
+	''' Helper-class for arr
+	'''
+
+	def __init__(self, file):
+		self._sourcefile = file
+		self._sources = self._load(self._sourcefile)
+		self._source = list(self._sources.keys())[1]
+		self._titles = None
+		self._title_of_feed = None
+		self._links = None
+		self._parser = rss_parser.Parser()
+
+
+	def _save(self, file, data):
+		with open(file, 'w') as f:
+			tmp = ''
+			
+			for key in data:
+				pattern = key +','+ data[key] +';'
+				tmp += pattern
+			
+			tmp = tmp[:-1]
+			f.write(tmp)
+
+			
+	def _load(self, file):
+		with open(file, 'r') as f:
+			tmp = f.read().strip().split(';')
+			d = dict()
+			
+			for item in tmp:
+				key, val = item.split(',')
+				d[key] = val
+			
+			return d
+
+
+	def add_source(self, key, addr):
+		'''	Add new feed to sources: key is name for dropdown-menu and 
+			addr is its URL-address.
+		'''
+		self._sources[key] = addr
+		
+		
+	def del_source(self, key=None):
+		'''	Key can be the name of the feed or index-number of the feed.
+			Remove one feed from sources.
+		'''
+		if isinstance(key, int): key = sorted(self._sources.keys())[key]
+		self._sources.pop(key)
+		
+
+	def show_sources(self):
+		''' Print all names and URLs of sources.
+		'''
+		for i,key in enumerate(sorted(self._sources.keys())):
+			print('{}: {}\t{}'.format(i, key, self._sources[key]))
+			
+
+	def current_source(self):
+		''' Print the name and URL of current feed.
+		'''
+		print(self._source, self._sources[self._source])
+		
+
+	def select_source(self, key):
+		''' Key can be the name of the feed or index-number of the feed.
+		'''
+		if isinstance(key, str):
+			self._source = key    
+		else:
+			self._source = sorted(self._sources.keys())[key]
+		
+		try: self._title_of_feed, self._titles, self._links = self._parser.parse(self._sources[self._source])
+		except (OSError, ValueError): raise
+
